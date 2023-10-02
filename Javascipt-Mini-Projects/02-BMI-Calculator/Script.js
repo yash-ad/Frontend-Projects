0b87e19754c928e9616a6be375e60629910a7c49
Yash <yashrajdandnaik@gmail.com>
2023-10-02 16:27:17 +0530

    .

--- Javascipt-Mini-Projects/02-BMI-Calculator/Script.js
+++ Javascipt-Mini-Projects/02-BMI-Calculator/Script.js
@@ -16,9 +16,9 @@ const results = document.querySelector('#results');
 //If-else if conditions to check the values for height and weight:-
 if(height === "" || height < 0 || isNaN(height)){
 //InnerHtml for values
-results.innerHTML = `Please provide a valid height! ${height}`;
+results.innerHTML = `Please provide a valid input! ${height}`;
 }else if(weight === "" || weight < 0 || isNaN(weight)){
-    results.innerHTML = `Please provide a valid weight! ${weight}`;
+    results.innerHTML = `Please provide a valid input! ${weight}`;
 }
 else{
 //The formula for BMI is weight in kilograms divided by height in meters squared.
