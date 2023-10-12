b6fbe1027576735e8c8313255658647f1911511f
Yash <yashrajdandnaik@gmail.com>
2023-10-12 20:46:48 +0530

    I have completed the 'five digit counter' project and gained knowledge on several concepts such as `DOMContentLoaded` , document,`.addEventListeners`,`.toString()` method,`.padStart()` string method,`parseInt()` method,`.value` property,setInterval(),clearInterval(),

--- Javascipt-Mini-Projects/07-FivedigitupCounter/Script.js
+++ Javascipt-Mini-Projects/07-FivedigitupCounter/Script.js
@@ -1,5 +1,7 @@
-//Declared all the variable and assigned them in their values.
-const digitElements = document.querySelectorAll('#counter span');
+document.addEventListener('DOMContentLoaded',function(){
+  
+  //Declared all the variable and assigned them in their values.
+  const digitElements = document.querySelectorAll('#counter span');
 const valueInput = document.getElementById('value');
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
@@ -16,7 +18,7 @@ for (let i = 0; i < 5; i++) {
 
 function startCounter(){
   const value = parseInt(valueInput.value,10);
-  if(isNan(value)|| value < 1 || value > 99999){
+  if(isNaN(value)|| value < 1 || value > 99999){
     alert('Please enter a valid value (1-99999)');
     return;
   }
@@ -40,10 +42,10 @@ function resetCounter(){
   location.reload();
 }
 
-
-
-
-
+startButton.addEventListener('click',startCounter);
+stopButton.addEventListener('click', stopCounter);
+resetButton.addEventListener('click', resetCounter);
+});
 
 
 
@@ -95,10 +97,28 @@ function resetCounter(){
 //isNaN(value): This checks if the value is not a valid number.
 // isNaN stands for "is Not a Number". It's a JavaScript function that returns true if the provided value cannot be converted into a number, and false otherwise. For example, isNaN('hello') returns true because 'hello' is not a valid number.
 // In this context, it's used to make sure the user has entered a valid numerical value in the input field. If value is not a number, the condition will evaluate to true.
-
 // value < 1: This checks if the value is less than 1.
-
 // This part of the condition ensures that the entered value is greater than or equal to 1. If value is less than 1, the condition will evaluate to true.
 // value > 99999: This checks if the value is greater than 99999.
-
 // This part of the condition ensures that the entered value is less than or equal to 99999. If value is greater than 99999, the condition will evaluate to true.
+
+// let currentValue = 0;: This initializes a variable named currentValue and sets it to 0. This variable will keep track of the current value of the counter.
+// intervalId = setInterval(function() { ... }, 1000);: This sets up a repeating interval, where the function inside the setInterval will be executed every 1000 milliseconds (1 second).
+// setInterval is a JavaScript function that repeatedly calls a function or executes a code snippet, with a fixed time delay between each call.
+// In this case, it's used to create a timer that will increment the counter value.
+// The function inside setInterval:
+// if (currentValue < value) { ... }: This condition checks if the currentValue is less than the desired value. If so, it means we haven't reached the target value yet.
+// currentValue++;: If the condition is met, currentValue is incremented by 1. This simulates the counting up process.
+// updateCounter(currentValue);: After incrementing the currentValue, it calls the updateCounter function to update the display with the new value.
+// else { clearInterval(intervalId); }: If the condition is not met (i.e., currentValue is now greater than or equal to value), it clears the interval using clearInterval. This stops the counting process.
+
+// document.addEventListener('DOMContentLoaded', function() { ... }); is an event listener in JavaScript. It's used to execute a function once the DOM (Document Object Model) content is fully loaded and ready for manipulation.
+//document: This refers to the HTML document that is currently loaded in the browser.
+//`.addEventListener(...)`: 
+// This is a method of the document object that allows you to specify a function to be executed when a certain event occurs.
+
+//'DOMContentLoaded': 
+//This is the event type being listened for. 
+//It fires when the initial HTML document has been completely loaded and parsed, 
+//without waiting for stylesheets, images, and subframes to finish loading.
+//This event is especially useful because it allows you to start executing JavaScript code as soon as the basic structure of the page is ready, without waiting for all external resources.
