d2e9d404b9516bf9f9435dad8bd27b21a9dc9b4f
Yash <yashrajdandnaik@gmail.com>
2023-10-26 20:58:48 +0530

    .

--- Javascipt-Projects/13-TodolistApp/script.js
+++ Javascipt-Projects/13-TodolistApp/script.js
@@ -1,44 +1,54 @@
-//1.Declare reference variables:-
-const newTaskInput = document.querySelector("#task-input");
-const addButton = document.querySelector("#add-btn");
-const taskContainer = document.querySelector("#task-container");
-const countValue = document.querySelector(".count-value")
-const showError = document.getElementById("error");
+const inputBox = document.getElementById('input-box');
+const addButton = document.getElementById('add-btn');
+const listContainer = document.getElementById('list-container');
+const showError = document.getElementById('error');
 
-let taskCount = 0; //To keep track of numbers.
-
-
-const displayCount = (taskCount)=>{
-    countValue.innerHTML = taskCount;
-}
-
-
-const addTask = ()=>{
-    const taskName =  newTaskInput.value.trim();
-    showError.style.display = "none";
-showError.innerHTML = "This field cannot be left empty,Please enter valid input"
-    if(taskName === ""){
+function addTask(){
+    if(inputBox.value === ""){
+        // alert("You must write something!");
+        showError.style.display = "block"
+        showError.innerHTML = "You must write something!";
         setTimeout(()=>{
-            showError.style.display = "block" 
-           
-        },200);
-    
-        return;
+showError.style.display = "none"
+        },2000)
+      
     }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+inputBox.value = ""; 
+saveData()
+};
 
-    const task = `<div class="task">
-    <input type="checkbox" id="task-checkbox">
-<span class="taskname">${taskName}</span>
-<button class="edit">
-<i class="fa-solid fa-pen-to-square"></i>
-</button>
-<button class="delete">
-<i class="fa-solid fa-trash"></i> 
-</button>
-    </div`;
+addButton.addEventListener('click',addTask);
 
-taskContainer.insertAdjacentHTML("beforeend", task);
+listContainer.addEventListener('click',(event)=>{
+if(event.target.tagName === "LI"){
+    console.log(event); //To check in the console if its working or not.
+    event.target.classList.toggle("checked");
+    saveData()  //When the tasks is checked the saveData() function will be called.
+}
+else if(event.target.tagName === "SPAN"){
+event.target.parentElement.remove();
+saveData()  //When the tasks is unchecked and wants to remove  the saveData() function will be called.
+}
+},false);
+
+//This function represents a feature in this app , that is when we clode the browser or reload or refresh the browser, the task we have added it remains as it is.
+//saveData() function save the data in the localStorage and update the content in the browser
+function saveData(){
+localStorage.setItem("data",listContainer.innerHTML)
 };
 
 
-addButton.addEventListener('click',addTask) //When the user clicks on the `Add` button the `addTask` function will be called.
+//Display the data Whenever we open the website again.
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+
+};
+showTask();
