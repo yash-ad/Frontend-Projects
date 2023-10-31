4a8c76aa74f7ac7f5a9026d292b55ecf36727895
Yash <yashrajdandnaik@gmail.com>
2023-10-31 17:49:53 +0530

    I completed the project of weather application, and i studied a lot of concepts from this project such as promises , asynchronous operations whereas fetch API,Async,await,`.json()`and eventlisteners

--- Javascipt-Projects/15-WeatherApp/script.js
+++ Javascipt-Projects/15-WeatherApp/script.js
@@ -1,3 +1,4 @@
+    ///.Variables declaration:-
     const searchBox = document.querySelector('.search input');
     const searchButton = document.querySelector('.search button');
     const weatherIcon = document.querySelector('.weather-icon');
@@ -7,9 +8,19 @@
      backgroundImage.src = 'https://source.unsplash.com/1600x900/?landscape';
 
 
+    ///.Function declaration:-
     async function checkWeather(city){
         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
+        //To check the condition if the cityname is valid or not:-
+        //Error message for Suppose we enter the wrong city name or invalid city name.
+        if(response.status == 404 ){
+            document.querySelector('.error').style.display = "block";
+            setTimeout(()=>{
+                document.querySelector('.error').style.display = "none";
+},2000)
+        }
+        else{
       let data = await response.json();
       console.log(data);
     let { name } = data;
@@ -48,16 +59,49 @@ else if(data.weather[0].main == "Mist"){
 else if(data.weather[0].main == "Rain"){
     weatherIcon.src = "Images/rain.png";
 }
+      //To clear the input box.
       searchBox.value = "";
-    };
-    
+}
+
+};
+ 
+
+///.Event listeners:-
+
+//1.When pressing the `Enter` Key on keyboard:-
 searchBox.addEventListener('keyup',(event)=>{
 if(event.key == "Enter"){
     checkWeather(searchBox.value);
 }
 });
 
+//2.When clicking the search-icon button:-
 searchButton.addEventListener('click',()=>{
         checkWeather(searchBox.value);
     })
   
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///.NOTES:-
+
+//1.searchBox, searchButton, and weatherIcon are variables that store references to specific elements in your HTML document using querySelector.
+// apiKey and apiUrl are constants that hold your OpenWeatherMap API key and the base URL for the weather API.
+// backgroundImage is an Image object that is preloaded with a default landscape image from Unsplash.
+
+//2.This function is responsible for fetching weather data from the OpenWeatherMap API based on a provided city name.
+// It starts by making an asynchronous fetch request to the API.
+// If the response status is 404 (city not found), it displays an error message for 2 seconds, then hides it.
+// Otherwise, it processes the response data.
+// It extracts relevant weather information (name, description, temperature, humidity, and wind speed).
+// It updates various elements in your HTML document with this information.
+// It sets the background image to a city-specific image using Unsplash.
+// It updates the weather icon based on the weather condition.
+// Finally, it clears the search box.
+
+//3.The first event listener triggers the checkWeather function when the Enter key is pressed in the search box.
+// The second event listener triggers the checkWeather function when the search button is clicked.
+
+
+
+
+
+
